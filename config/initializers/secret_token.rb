b00b187ea6c901c92ae66bf5994fb89758143802
Shopno Buz <info@shopnobuz.com>
2012-12-26 12:41:31 +0600

    Initial Rails Blank Apps

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SavarCantt::Application.config.secret_token = 'c7bc22588e19329f2995ea858711891c78e60dbbb197a8bcb49a025c3a3481abe57179e15c4f5d7a88c00f3507ca1c57cb5281dfd48d713f311aba6a11a8aa86'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SavarCantt::Application.config.session_store :cookie_store, key: '_savar-cantt_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# SavarCantt::Application.config.session_store :active_record_store
